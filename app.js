b61f9a201aaa92f1ea80503a0f9e90d1959a0747
Dorcasliu <43295677+Dorcasliu@users.noreply.github.com>
2020-11-19 02:20:20 +1100

    add getting details of the movie

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-const express=require("express");
+const express = require("express");
 const bodyParser = require("body-parser");
+const request = require('request');
+const http = require("http");
 
-const app=express();
+
+const app = express();
 app.use(bodyParser.urlencoded({
   extends: true
 }));
@@ -10,13 +13,45 @@ app.use(express.static("public"));
 
 
 app.get("/", function(req, res) {
-  res.render("result");
+  res.render("search");
 });
 
-app.post("/",function(req,res){
+app.get("/detail",function(req,res){
+  const query = req.query.name;
+  const apikey = "26555fd4";
+  const url = "http://www.omdbapi.com/?apikey=" + apikey + "&t=" + query;
+  request(url, function(error,response,body){
+    if(!error && response.statusCode==200){
+      var data=JSON.parse(body)
+      res.render("detail",{data:data});
+    }
+  })
 
 });
 
-app.listen(3000,function(){
-  console.log("port 3000 is now in listening");
-});
+app.post("/", function(req, res) {
+      const query = req.body.search;
+      const apikey = "26555fd4";
+      const url = "http://www.omdbapi.com/?apikey=" + apikey + "&s=" + query;
+      request(url, function(error,response,body){
+        if(!error && response.statusCode==200){
+          var data=JSON.parse(body)
+          res.render("result",{data:data});
+        }
+      })
+
+      //second method
+      // http.get(url, function(response) { //connect to others' server
+      //   response.on("data", function(data) {
+      //       const newData = JSON.parse(data)
+      //       res.render("result", {
+      //         data: newData
+      //       });
+      //     })
+      //   })
+
+      });
+
+      app.listen(3000, function() {
+        console.log("port 3000 is now in listening");
+      });
